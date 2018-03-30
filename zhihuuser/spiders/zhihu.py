b1522580573ab14d59b9fe72d8e3f758ba8d3387
Wang Yu <1024394026@qq.com>
2018-03-30 20:17:57 +0800

    zhihu

---
+++ zhihuuser/spiders/zhihu.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import json
+
+from scrapy import Spider, Request
+
+from zhihuuser.items import ZhihuuserItem
+
+
+class ZhihuSpider(Spider):
+    name = 'zhihu'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['http://www.zhihu.com/']
+
+    start_user = 'excited-vczh'
+    user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
+    user_query = 'allow_message,is_followed,is_following,is_org,is_blocking,employments,answer_count,follower_count,articles_count,gender,badge[?(type=best_answerer)].topics'
+
+    follows_url = 'https://www.zhihu.com/api/v4/members/{user}/followees?include={include}&offset={offset}&limit={limit}'
+    follows_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
+
+    def start_requests(self):
+        yield Request(self.user_url.format(user=self.start_user, include=self.user_query), self.parse_user)
+        yield Request(self.follows_url.format(user=self.start_user, include=self.follows_query, offset=0, limit=20),
+                      self.parse_follows)
+
+    def parse_user(self, response):
+        result = json.loads(response.text)
+        item = ZhihuuserItem()
+
+    def parse_follows(self, response):
+        result = json.loads(response.text)
+        print(result)
